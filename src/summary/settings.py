ce108789a7a831111f350c840fd021db57cb78fc
Mouhamed Mbaye <mbaye.mouhamed020@gmail.com>
2025-12-22 21:09:32 +0000

    Configuration .env (variables)

--- src/summary/settings.py
+++ src/summary/settings.py
@@ -1,21 +1,19 @@
 from pathlib import Path
-import os
+import os, environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+environ.Env.read_env(env_file=str(BASE_DIR / "youtube" / '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c^&-knis60c-y%!-!qd(g5d)uj2asdb&q+(zr-h47e^*_=y&@-'
+SECRET_KEY = env('SECRET_KEY')
+GOOGLE_GENAI_API_KEY = env('GOOGLE_GENAI_API_KEY')
+DEBUG = env.bool('DEBUG', default=False)
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-GOOGLE_GENAI_API_KEY = 'AIzaSyCGu8zmbzbRgTri5ieunYH_3og6JieQFew'
 
 # Application definition
 
@@ -105,6 +103,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
